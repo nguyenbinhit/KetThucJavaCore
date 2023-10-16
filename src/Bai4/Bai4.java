b62c8923f6dafa3b893b85d266deb17294ab689b
Nguyen Binh <69001423+nguyenbinhit@users.noreply.github.com>
2023-10-16 19:02:44 +0700

    Update Bai4.java

--- src/Bai4/Bai4.java
+++ src/Bai4/Bai4.java
@@ -3,11 +3,11 @@ package Bai4;
 public class Bai4 {
 	public static void main(String[] args) {
 		for (int i = 100; i <= 999; i++) {
-			int digit1 = i / 100;
-            int digit2 = (i % 100) / 10;
-            int digit3 = i % 10;
+			int d1 = i / 100;
+            int d2 = (i % 100) / 10;
+            int d3 = i % 10;
 
-            if (digit1 + digit2 + digit3 == 10) {
+            if (d1 + d2 + d3 == 10) {
                 System.out.println("So gom 3 chu so co tong bang 10 la: " + i);
             }
 		}
